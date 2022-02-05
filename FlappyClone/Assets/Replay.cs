bade227a4c8c513515f6ca41d5e577ee01e2a8c2
krrong <khk9664@naver.com>
2022-02-05 17:53:50 +0900

    게임 재실행 코드

---
+++ FlappyClone/Assets/Replay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Replay : MonoBehaviour
+{
+    // 게임 재실행
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+}
