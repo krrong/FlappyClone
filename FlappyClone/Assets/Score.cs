1bec4db622d22e6ee51f372aa0c449c98f45d8ce
krrong <khk9664@naver.com>
2022-02-05 17:54:24 +0900

    점수 업데이트 및 초기화 코드

---
+++ FlappyClone/Assets/Score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// unity의 UI를 가져와 사용하기 위해 추가
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    // 외부에서 접근해서 사용하기 위해 static
+    public static int score = 0;
+    public static int bestScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 점수 초기화
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // int를 문자형 자료형으로 변경
+        GetComponent<Text>().text = score.ToString();
+    }
+}
