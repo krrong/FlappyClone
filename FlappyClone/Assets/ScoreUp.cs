d54dfeb492dfc5473f220567ad93d25e5f3fe71e
krrong <khk9664@naver.com>
2022-02-05 17:54:36 +0900

    통과할 때 점수 증가하는 코드

---
+++ FlappyClone/Assets/ScoreUp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreUp : MonoBehaviour
+{
+    // Trigger속성 --> Collider끼리 통과 가능
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // 통과할 때 점수 증가
+        Score.score++;
+    }
+}
