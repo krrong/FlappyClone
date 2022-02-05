32e15be7916490f271e4b760c92f879597fe4352
krrong <khk9664@naver.com>
2022-02-05 17:53:23 +0900

    게임 종료시 스코어 뜨도록 하는 코드

---
+++ FlappyClone/Assets/CurrentScore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 현재 스코어가 뜨도록 수정
+        GetComponent<Text>().text =  "Score : " + Score.score.ToString();
+    }
+
+}
