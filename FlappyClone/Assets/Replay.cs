9839e832e9b1d57356bb9f1cf8ec44407711474f
krrong <khk9664@naver.com>
2022-02-10 22:19:52 +0900

    전면 배너 광고 코드 추가

--- FlappyClone/Assets/Replay.cs
+++ FlappyClone/Assets/Replay.cs
@@ -2,12 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using GoogleMobileAds.Api;
 
 public class Replay : MonoBehaviour
 {
-    // 게임 재실행
+    private InterstitialAd interstitial;
+    public Canvas myCanvas;
+
+    // 전면 광고 요청 함수
+    private void RequestInterstitial()
+    {
+        #if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-7153366510996370/4348926086";
+        #elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+        #else
+            string adUnitId = "unexpected_platform";
+        #endif
+
+        // Initialize an InterstitialAd.
+        this.interstitial = new InterstitialAd(adUnitId);
+        // Called when the ad is closed.
+        this.interstitial.OnAdClosed += HandleOnAdClosed;
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        this.interstitial.LoadAd(request);
+    }
+
+    // 게임 재실행 함수
     public void ReplayGame()
     {
+
+        RequestInterstitial();
+        //When you want call Interstitial show
+        StartCoroutine(showInterstitial());
+
+        IEnumerator showInterstitial()
+        {
+            while (!this.interstitial.IsLoaded())
+            {
+                yield return new WaitForSeconds(0.2f);
+            }
+            this.interstitial.Show();
+            myCanvas.sortingOrder = -1;
+        }
+
+    }
+
+    // 광고가 꺼졌을 때
+    public void HandleOnAdClosed(object sender, System.EventArgs args)
+    {
+        // 게임 재실행
         SceneManager.LoadScene("PlayScene");
     }
 }
